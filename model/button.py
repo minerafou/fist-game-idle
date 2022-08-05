3e0b533471e152af65a8559478496d18685a24ff
minerafou <110623516+minerafou@users.noreply.github.com>
2022-08-05 13:42:57 +0200

    dig update #1. button
    fix black screen bug
    button class upgrade
    add new button

--- model/button.py
+++ model/button.py
@@ -1,14 +1,46 @@
 import pygame
+from model.money_display import money_display
 
 
 class button:
-    def __init__(self, x, y, width, height, color, screen):
+    def __init__(self, x, y, width, height, color, color_pos, screen, text, cost, earn, earn_ps, earn_pc):
         self.rect = pygame.Rect(x, y, width, height)
         self.color = color
+        self.color_pos = color_pos
         self.screen = screen
+        self.text = text
+        self.textfont = pygame.font.SysFont("monospace", 30)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.cost = cost
+        self.earn = earn
+        self.earn_ps = earn_ps
+        self.earn_pc = earn_pc
 
     def draw_button(self):
-        pygame.draw.rect(self.screen, self.color, self.rect)
+        #affiche le bouton
+        mouse_pos = pygame.mouse.get_pos()
+        text = self.textfont.render(self.text , True, (0, 0, 0))
+        text_rect = text.get_rect(center=(self.x + (self.width / 2), self.y + (self.height / 2)))
+        
+        if self.rect.collidepoint(mouse_pos):
+            pygame.draw.rect(self.screen, self.color, self.rect)
+        else:
+            pygame.draw.rect(self.screen, self.color_pos, self.rect)
+        self.screen.blit(text, text_rect)
 
     def button_pressed(self):
-        pass
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            return True
+        else:
+            return False
+
+    def add_earn_pc(self, increase):
+        self.earn += increase
+
+    def get_action(self):
+        self.action = (self.cost, self.earn, self.earn_ps, self.earn_pc)
+        return self.action
--- model/game.py
+++ model/game.py
@@ -5,6 +5,8 @@ from model.button import button
 #creation de la class 'game'
 class game:
     def __init__(self, screen):
+        #creation de la liste de boutton
+        self.buttons = []
         #recuparation de la variable screen
         self.screen = screen
         #mise en place de la clock
@@ -12,8 +14,11 @@ class game:
         #mise en place des variable
         self.running = True
         self.money_display = money_display()
-        self.money_click = 1
-        self.button1 = button(20, 100, 60, 60, (100, 100, 100), self.screen)
+        self.money = 0
+        self.money_ps = 0
+        self.money_pc = 1
+        self.buttons.append(button(20, 100, 60, 60, (150, 150, 150), (180, 180, 180), self.screen, "+1", 0, 1, 0, 0))
+        self.buttons.append(button(20, 200, 320, 60, (150, 150, 150), (180, 180, 180), self.screen, "cost:10 pc+0.1", 10, 0, 0, 0.1))
 
     def check_event(self):
         #verifie les input du joueur
@@ -21,24 +26,34 @@ class game:
             #input de la croix rouge (en haut a droite de la fenetre)
             if event.type == pygame.QUIT:
                 self.running = False
+            
             #input de la souris
             if event.type == pygame.MOUSEBUTTONDOWN:
-                self.money_display.earn_money(self.money_click)
-                print("mouse")
-        print("check event")
+                for i in self.buttons:
+                    if i.button_pressed():
+                        cost, money_to_earn, increase_money_ps, increase_money_pc = i.get_action()
+                        print(money_to_earn)
+                        if self.check_for_money(cost):
+                            self.earn_money(money_to_earn)
+                            self.buy_money(cost)
+                            self.increase_money_ps(increase_money_ps)
+                            self.increase_money_pc(increase_money_pc)
+                        
 
     def update(self):
         #delete tous sur l'ecran
         self.screen.fill((220, 220, 220))
+
         #affiche les different trucs
-        self.money_display.draw_money_display(self.screen)
-        self.button1.draw_button()
-        print("update")
+        #affachage de l'argent
+        self.money_display.draw_money_display(self.screen, self.money)
+        #affichage des boutons
+        for i in self.buttons:
+            i.draw_button()
 
     def refresh(self):
         #refresh l'ecran
         pygame.display.flip()
-        print("refresh")
 
     def run(self):
         #boucle global du jeu
@@ -47,4 +62,23 @@ class game:
             self.update()
             self.refresh()
             self.clock.tick(60)
-            print("boucle")
+
+    def check_for_money(self, money_to_check):
+        if self.money >= money_to_check:
+            return True
+        else:
+            return False
+
+    def earn_money(self, money_to_earn):
+        self.money += money_to_earn
+
+    def buy_money(self, price):
+        self.money -= price
+    
+    def increase_money_ps(self, increase):
+        self.money_ps += increase
+    
+    def increase_money_pc(self, increase):
+        self.buttons[0].add_earn_pc(increase)
+        self.money_pc += increase
+    
